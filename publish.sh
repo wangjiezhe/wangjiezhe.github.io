cf9470cf45b5b07ccf8f6f0a5a7b46a5818c3998
wangjiezhe <wangjiezhe@gmail.com>
2018-10-05 04:42:28 +0800

    Fix: Travis-CI

--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
 
-git clone https://%{GH_REF} .deploy_git
+git clone https://${GH_REF} .deploy_git
 cd .deploy_git
 git checkout master
 
@@ -15,4 +15,4 @@ git config user.email wangjiezhe@gmail.com
 
 git add .
 git commit -m "Travis CI Auto Builder at $(date +'%Y-%m-%d %H:%M:%S')"
-git push --force --quiet "https://${GH_TOKEN}@{GH_REF}" master:master
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:master
