050ec29923948d41d98e4c6321c570cb69619053
wangjiezhe <wangjiezhe@gmail.com>
2019-12-28 04:35:55 +0800

    Travis CI Auto Builder at 2019-12-28 04:35:55

--- js/crash-cheat.js
+++ js/crash-cheat.js
@@ -1,17 +1,1 @@
-<!--崩溃欺骗-->
- var OriginTitle = document.title;
- var titleTime;
- document.addEventListener('visibilitychange', function () {
-     if (document.hidden) {
-         $('[rel="icon"]').attr('href', "/images/favicon-32x32-next.png");
-         document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
-         clearTimeout(titleTime);
-     }
-     else {
-         $('[rel="icon"]').attr('href', "/images/favicon-32x32-next.png");
-         document.title = '(ฅ>ω<*ฅ) 噫又好了~' + OriginTitle;
-         titleTime = setTimeout(function () {
-             document.title = OriginTitle;
-         }, 2000);
-     }
- });
+var titleTime,OriginTitle=document.title;document.addEventListener("visibilitychange",function(){document.hidden?($('[rel="icon"]').attr("href","/images/favicon-32x32-next.png"),document.title="╭(°A°`)╮ 页面崩溃啦 ~",clearTimeout(titleTime)):($('[rel="icon"]').attr("href","/images/favicon-32x32-next.png"),document.title="(ฅ>ω<*ฅ) 噫又好了~"+OriginTitle,titleTime=setTimeout(function(){document.title=OriginTitle},2e3))});
